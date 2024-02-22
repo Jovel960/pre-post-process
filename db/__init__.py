7abb065ad65dfb9ad3701b9a346700fd78ce21a8
Jovel960 <jovel960@icloud.com>
2024-02-22 15:37:37 +0200

    save the adjusted anootation method on db

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,4 @@
 from db.sqlite import (connectToDb)
-from db.sqlite.trainData import (fetch_images_url, save_dims)
+from db.sqlite.trainData import (fetch_images_url, save_dims, save_adjusted_annotation)
 
 connectToDb()
